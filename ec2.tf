25969e509766d7c0b4e6e3b93ca05bba2d3cbde0
Ubuntu <ubuntu@ip-172-31-34-46.ap-south-1.compute.internal>
2024-06-29 13:24:41 +0000

    Added Policy into Role

--- ec2.tf
+++ ec2.tf
@@ -21,6 +21,10 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
   role = aws_iam_role.ecs_instance_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_instance_policy_attachment" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
 
 // Launch Template
 resource "aws_launch_template" "ecs_lt" {
